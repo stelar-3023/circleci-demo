e44fa5b24b0b2c767f3251f6538b646ea8eea4d1
Steve Larsen <slarsen-3@att.net>
2022-01-27 16:47:25 -0600

    removed failing test

--- main-test.py
+++ main-test.py
@@ -1,11 +1,11 @@
 # Import the add_number function from main.py, and assert that it returns the correct value.
 from main import add_number
 
+
 def test_add():
     assert add_number(1, 2) == 3
-    assert add_number(5, 5) == 11
     print("Test passed!")
+
 
 if __name__ == "__main__":
     test_add()
-    
